ec276ab3af76b225f5342a3ab20ee75a48a4aaba
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2023-11-26 01:48:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const DEFAULT_PORT_HTTP = "22023";
-const DEFAULT_PORT_HTTPS = "443";
-
-/**
- * @typedef {{ address: string, port: number, name: string, protocol: string, url: string }} ServerInfo
- * @returns {ServerInfo}
- */
-function parseForm() {
-    return false;
-}
-
-async function downloadAsync() {
-    return false;
-}
-
 async function openApp() {
     window.location = 'amongus://init?servername=Modded_SA&serverport=22023&serverip=http%3A%2F%2F34.95.199.244&usedtls=false';
     return false;
